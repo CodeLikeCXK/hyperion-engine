3b2b63697337b5ae8ecd66af88e5d0b812adaa83
CodeLikeCXK <44729188+CodeLikeCXK@users.noreply.github.com>
2025-08-04 20:50:41 +0800

    Batch files for windows arm64

---
+++ build - arm64.bat
@@ -0,0 +1,19 @@
+@echo off
+
+REM Get the script directory
+SET SCRIPT_DIR=%~dp0
+REM Remove trailing backslash
+SET SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+
+SET BUILD_TOOL_CMD="%SCRIPT_DIR%\build\hyperion-buildtool.exe" --WorkingDirectory="%SCRIPT_DIR%" --SourceDirectory="%SCRIPT_DIR%\src" --CXXOutputDirectory="%SCRIPT_DIR%\build\generated" --CSharpOutputDirectory="%SCRIPT_DIR%\src\generated\csharp" --ExcludeDirectories="%SCRIPT_DIR%\src\generated" --ExcludeFiles="%SCRIPT_DIR%\src\core\Defines.hpp"
+
+ECHO Regenerate CMake? (will continue without regenerating in 3s)
+CHOICE /C YN /T 3 /D N /M "Press Y for Yes or N for No"
+
+IF ERRORLEVEL 2 (
+    echo n| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64.bat" %*
+) ELSE (
+    echo y| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64.bat" %*
+)
+
+pause
