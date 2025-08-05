23b6405aaffe888b1fc5c403cbaeed697ef8b26e
CodeLikeCXK <44729188+CodeLikeCXK@users.noreply.github.com>
2025-08-05 10:18:28 +0800

    Drop arm64ec support for arm64. Replace nethost files with arm64 version.

--- build - arm64.bat
+++ build - arm64.bat
@@ -7,13 +7,15 @@ SET SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 
 SET BUILD_TOOL_CMD="%SCRIPT_DIR%\build\hyperion-buildtool.exe" --WorkingDirectory="%SCRIPT_DIR%" --SourceDirectory="%SCRIPT_DIR%\src" --CXXOutputDirectory="%SCRIPT_DIR%\build\generated" --CSharpOutputDirectory="%SCRIPT_DIR%\src\generated\csharp" --ExcludeDirectories="%SCRIPT_DIR%\src\generated" --ExcludeFiles="%SCRIPT_DIR%\src\core\Defines.hpp"
 
+SET ALLOW_EXTERNAL_SPIRV_TOOLS=YES
+
 ECHO Regenerate CMake? (will continue without regenerating in 3s)
 CHOICE /C YN /T 3 /D N /M "Press Y for Yes or N for No"
 
 IF ERRORLEVEL 2 (
-    echo n| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64ec.bat" %*
+    echo n| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64.bat" %*
 ) ELSE (
-    echo y| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64ec.bat" %*
+    echo y| call "%SCRIPT_DIR%\tools\scripts\BuildHyperion_ARM64.bat" %*
 )
 
 pause
